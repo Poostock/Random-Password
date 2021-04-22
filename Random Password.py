86a562500894732d75bad7c881d4971c5869b917
Puttipong Kidrum <75108331+Poostock@users.noreply.github.com>
2021-04-22 23:23:07 +0700

    Update Random Password.py

--- Random Password.py
+++ Random Password.py
@@ -1,30 +1,28 @@
 import random
+
 print('your password is: ', end='')
 lt = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B',
-      'C','D','E','F','G','H','I','J','K','L','M','N',
-      'O','P','Q','R','S','T','U','V','W','X','Y','Z')
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+      'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+      'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 ae = random.choice(lt)
-LT =('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B',
-      'C','D','E','F','G','H','I','J','K','L','M','N',
-      'O','P','Q','R','S','T','U','V','W','X','Y','Z')
+LT = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+      'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+      'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 be = random.choice(LT)
 Lt = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B',
-      'C','D','E','F','G','H','I','J','K','L','M','N',
-      'O','P','Q','R','S','T','U','V','W','X','Y','Z')
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+      'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+      'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 ce = random.choice(Lt)
 lT = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B',
-      'C','D','E','F','G','H','I','J','K','L','M','N',
-      'O','P','Q','R','S','T','U','V','W','X','Y','Z')
+      'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+      'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+      'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
 de = random.choice(lT)
 
+values = str(random.randint(0, 9))
+valuel = str(random.randint(0, 9))
 
-values = str(random.randint(0,9))
-valuel = str(random.randint(0,9))
-
-
-
-print(ae+be+values+valuel+ce+de)
+print(ae + be + values + valuel + ce + de)
